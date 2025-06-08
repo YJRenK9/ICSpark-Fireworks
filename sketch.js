445dbbcaa7252ed0492f7bff925f18aa959943cf
YJRenK9 <jordan.quan.le@gmail.com>
2025-06-08 12:32:32 -1000

    fixing mobile issue

--- sketch.js
+++ sketch.js
@@ -433,6 +433,6 @@ function rainbowTransition() {
 }
 
 // used to let users launch fireworks via tapping
-// document.ontouchmove = function(event) {
-//   event.preventDefault();
-// }
+document.ontouchmove = function(event) {
+  event.preventDefault();
+}
